18e5f161349c5a751b196bbdb821c26d9f314b80
Pasetti <pasetti@pnp-software.com>
2022-01-29 22:24:28 +0100

    Removed unused filter functions

--- editor/templatetags/ref_filters.py
+++ editor/templatetags/ref_filters.py
@@ -85,31 +85,6 @@ def disp_trac(context, spec_item, trac_cat, trac_link):
  
  
 @register.filter(is_safe=True)
-def filter_ver_items(tc):
-    """
-    The argument is a test case record.
-    The filter returns a string holding a description of the items verified by the test case.
-    """
-    return mark_safe(render_ver_items_for_display(tc))
-
-
-@register.filter(is_safe=True)
-def filter_di_value(val):
-    """ Parse the mathematical expression in a data item value and return its numerical value. """
-    return mark_safe(eval_di_value(val))
-
-
-@register.filter(is_safe=True)
-def filter_refs(s):
-    """
-    The argument is a string which contains references and must be rendered in an html page.
-    The input should be passed through escape() before being filtered to ensure that any html code entered 
-    by the (possibly malicious) user has been sanitized.
-    """
-    return mark_safe(convert_db_to_display(s))
-
-
-@register.filter(is_safe=True)
 def filter_expand_tip(spec_item):
     """
     The argument is a spec_item which appears in an "expand" list.
