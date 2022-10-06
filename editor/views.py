2b754f2da23692aeb873ca0da8110c5d4888d5c3
Pascal Stump <pascal.stump@pnp-software.com>
2022-10-06 08:25:10 +0200

    add spec item dropdown in breadcrumb
    Closes: #84

--- editor/views.py
+++ editor/views.py
@@ -386,9 +386,11 @@ def list_spec_items(request, cat, project_id, application_id, val_set_id, sel_va
     n_pad_fields = range(len(configs['cats'][cat][disp])-3)
 
     if application_id != 0:
-        application_name = Application.objects.get(id=application_id)
+        application = Application.objects.get(id=application_id)
+        cats = application.cats.split(', ')
     else:
-        application_name = None
+        application = None
+        cats = project.cats.split(', ')
     
     if (configs['cats'][cat]['level'] == 'project') or (application_id == 0):   
         items = SpecItem.objects.filter(project_id=project_id).filter(cat=cat).filter(val_set_id=val_set_id).\
@@ -415,9 +417,9 @@ def list_spec_items(request, cat, project_id, application_id, val_set_id, sel_va
     else:
         expand_items = None
     
-    context = {'items': items, 'project': project, 'application_id': application_id, 'application_name': application_name, 'domains': domains, 'sel_val': sel_val,\
+    context = {'items': items, 'project': project, 'application_id': application_id, 'application': application, 'domains': domains, 'sel_val': sel_val,\
                'val_set': val_set, 'val_sets': val_sets, 'default_val_set_id': default_val_set.id, \
-               'config': configs['cats'][cat], 'cat': cat, 'expand_id': expand_id, \
+               'config': configs['cats'][cat], 'config_cats': configs['cats'], 'cat': cat, 'cats': cats, 'expand_id': expand_id, \
                'expand_items': expand_items, 'expand_link': expand_link, 'n_pad_fields': n_pad_fields, \
                'disp': disp, 'disp_list':configs['cats'][cat][disp], 'history': False, 'order_by': order_by }
     return render(request, 'list_spec_items.html', context)    
